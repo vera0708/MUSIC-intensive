78285ede89e5991c1bf0955acc312c39790e3ea6
Vera Deeva <verasdee@gmail.com>
2022-11-04 11:10:55 +0100

    done playing  music js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 const audio = new Audio();
+const tracksCard = document.getElementsByClassName('track');
+console.log('tracksCard: ', tracksCard);
 const pauseBtn = document.querySelector('.player__icon_pause');
 
-const playMusic = url => {
-    audio.src = url;
+const playMusic = (evt) => {
+    // console.dir(evt.currentTarget);
+    audio.src = event.currentTarget.dataset.track;
     audio.play();
 }
-pauseBtn.addEventListener('click', () => {
-    playMusic('audio/Adele - Hello.mp3')
-});
+
+for (let i = 0; i < tracksCard.length; i++) {
+    tracksCard[i].addEventListener('click', playMusic)
+};
+
+
+// pauseBtn.addEventListener('click', () => {
+//     playMusic('audio/Adele - Hello.mp3')
+// });
