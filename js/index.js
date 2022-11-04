241f15fc4db32e373a3df3c9079257d9abd3a5ed
Vera Deeva <verasdee@gmail.com>
2022-11-04 15:49:36 +0100

    add function to music js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
 const audio = new Audio();
 const tracksCard = document.getElementsByClassName('track');
-console.log('tracksCard: ', tracksCard);
+const player = document.querySelector('.player');
 const pauseBtn = document.querySelector('.player__icon_pause');
+const stopBtn = document.querySelector('.player__icon_stop');
 
 const playMusic = (evt) => {
     // console.dir(evt.currentTarget);
-    audio.src = event.currentTarget.dataset.track;
+    const trackActive = evt.currentTarget;
+
+    audio.src = trackActive.dataset.track;
     audio.play();
-}
+    pauseBtn.classList.remove('player__icon_play');
+    player.classList.add('player_active');
+    for (let i = 0; i < tracksCard.length; i++) {
+        tracksCard[i].classList.remove('track_active');
+    }
+
+    trackActive.classList.add('track_active')
+};
 
 for (let i = 0; i < tracksCard.length; i++) {
     tracksCard[i].addEventListener('click', playMusic)
 };
 
 
-// pauseBtn.addEventListener('click', () => {
-//     playMusic('audio/Adele - Hello.mp3')
-// });
+pauseBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        pauseBtn.classList.remove('player__icon_play')
+    } else {
+        audio.pause();
+        pauseBtn.classList.add('player__icon_play')
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    // скрыть и удалить из src трек 
+})
